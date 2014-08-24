268cc8f48f91b9f4a368948fe2446376a79c81c6
Faraz Oman <farazoman@gmail.com>
2014-08-24 16:43:33 -0400

    changes made on bus

--- term.js
+++ term.js
@@ -1,27 +1,62 @@
 var prompt = "";
 var cmds = {};
-cmds["testing"] = function(){test()};
+cmds["help"] = function(){help()};
+cmds["whoru"] = function(){info()};
+cmds[""] = function(){nada()};
+var welcomemsg = "Welcome to my website, you may enter the graphical user interface by typing the command 'enter' and if you wish to use the terminal type 'help' for more commands."
+var info_v = "</br>I am Faraz oman";
+var errmsg = "</br>Sorry that is not valid command, refer to the valid commads below.</br>";
 var bk_colour;
 var txt_colour;
 var cmd = "";
 var endline = "</br>";
 var blinkerOn = false;
 
-
 function load(prompt_){
-	prompt += prompt_;
+	var termdiv = document.getElementById("term_area");
 
-    var termdiv = document.getElementById("term_area");
-    termdiv.innerHTML = prompt;
+	welcome();
 
     prompt = endline;
     prompt += prompt_;
-    cmds["testing"]();
+
+    termdiv.innerHTML += prompt;
+    
     setInterval(function(){blinker()}, 500);
 }
 
-function test(){
-	alert("asdf");
+function nada(){}
+
+function add_to_div(text){
+	var termdiv = document.getElementById("term_area");
+	var str = termdiv.innerHTML;
+	str += text;
+	termdiv.innerHTML = str;
+}
+
+function welcome(){
+	add_to_div(welcomemsg);
+}
+
+function info(){
+	add_to_div(info_v);
+}
+
+function help(){
+	var ayudar = "</br>The available commands are:"
+
+	for(i in cmds){
+		ayudar += "</br>";
+		ayudar += i;
+	}
+
+	add_to_div(ayudar);
+
+}
+
+function error(){
+	add_to_div(errmsg);
+	help();
 }
 
 function blinker(){
@@ -34,7 +69,7 @@ function blinker(){
     	ltr += '|';
     	blinkerOn = true;
     }
-    document.getElementById("term_area").innerHTML += ltr;
+    add_to_div(ltr);
 }
 
 //Event listener for keypressed
@@ -43,18 +78,23 @@ document.addEventListener('keypress', function(event) {
 	//When enter is pressed, process input
     if(event.keyCode == 13) {
     	//process cmd here!!!
+    	backspace(true);
+    	try{
+    		cmds[cmd]();
+    	}
+    	catch(err){
+    		error();
+		}
 
 		backspace(true);
-		blinkerOn = false;
     	cmd="";
-    	document.getElementById("term_area").innerHTML += prompt; 
+    	add_to_div(prompt); 
     }
     else{
     	var ltr = String.fromCharCode(event.keyCode);
-    	
     	backspace(true);
     	blinkerOn = false;
-    	document.getElementById("term_area").innerHTML += ltr; 
+    	add_to_div(ltr)
 
     	cmd += ltr;
     	
@@ -66,6 +106,7 @@ function backspace(blinkerOnly){
    	
    	if(blinkerOn){
 		str = str.substring(0, str.length - 1);
+		blinkerOn = false;
 	}
 
 	if(cmd.length != 0 && !blinkerOnly){
