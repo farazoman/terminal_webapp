12f09691a8a4d61f87ce5be282914ae20eaed704
Faraz Oman <farazoman@gmail.com>
2014-08-25 07:01:58 -0400

    added email link and clear function

--- js/term.js
+++ js/term.js
@@ -5,10 +5,11 @@ cmds["whoami"] = function(){info()};
 cmds["myprojs"] = function(){projects()};
 cmds["enter"] = function(){enter()};
 cmds["version"] = function(){version()};
+cmds["clear"] = function(){clear()};
 cmds[""] = function(){nada()};
 var version_ = "webterminal v0.2.0";
 var welcomemsg = "Welcome to my website, you may enter the graphical user interface by typing the command 'enter' and if you wish to use the terminal type 'help' for more commands."
-var info_v = "</br></br>My name is Faraz Oman, and I am a student at McGill University, Majoring in Software Engineering and Minoring in Mathematics. I am currently entering my second year and I am a part of a veritiy of extracurriculars. I am the editor-in-cheif of the <a target='_blank' href='http://www.plumbersfaucet.ca'>Plumber's Faucet</a> and I am regularly involved in McGill Engineering Undergraduate Society events.</br></br>I enjoy to spend my time listening to music, learning spanish and biking.</br></br>I can be reached at farazoman@gmail.com if you wish to get in contact with me. Enjoy the rest of the site.</br>";
+var info_v = "</br></br>My name is Faraz Oman, and I am a student at McGill University, Majoring in Software Engineering and Minoring in Mathematics. I am currently entering my second year and I am a part of a veritiy of extracurriculars. I am the editor-in-cheif of the <a target='_blank' href='http://www.plumbersfaucet.ca'>Plumber's Faucet</a> and I am regularly involved in McGill Engineering Undergraduate Society events.</br></br>I enjoy to spend my time listening to music, learning spanish and biking.</br></br>I can be reached at <a target='_blank' href='mailto:farazoman@gmail.com'>farazoman@gmail.com</a> if you wish to get in contact with me. Enjoy the rest of the site.</br>";
 var errmsg = "</br></br><span id='err'>Sorry</span> that is not valid command, refer to the valid commads below.";
 var proj_info = "</br></br>Hi there, thanks for the interest, my projects can be found at my <a target='_blank' href='http://www.github.com/farazoman'>github</a> also visit <a target='_blank' href='http://finance.mcgilleus.ca'>here</a> to get a glimpse of the project headed by me. The mcgilleus project is made to digitize the submission and authentication of cheque requisions for the Engineering Undergraduate Society of McGill university. On github you can view projects that I have worked on from highschool including a few things I have worked on including this terminal style interface.</br>"
 var bk_colour;
@@ -20,6 +21,7 @@ var blinkerOn = false;
 function load(prompt_){
 	var termdiv = document.getElementById("term_area");
 
+    termdiv.innerHTML = "";
 	welcome();
 
     prompt = endline;
@@ -55,6 +57,11 @@ function version(){
     var version_info = "</br></br>" + version_ + "</br>This version is still in beta with many features still to be implemented. Source code can be viewed <a target='_blank' href='http://www.github.com/farazoman/terminal_webapp'>here</a></br>";
 
     add_to_div(version_info);
+}
+
+function clear(){
+    var termdiv = document.getElementById("term_area");
+    termdiv.innerHTML = "";
 }
 
 function help(){
