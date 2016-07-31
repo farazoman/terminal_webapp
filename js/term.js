17326ab31b796f0e9a29c41a1283141171b4be99
Faraz Oman <faraz@farazoman.com>
2016-07-31 15:53:54 -0400

    updated how a command-text is shown and stored and up and down keys traverse history

--- js/term.js
+++ js/term.js
@@ -12,7 +12,7 @@ cmds[""] = function(){nada()};
 var version_ = "webterminal v0.3.0";
 //var info_v = "</br></br>My name is Faraz Oman, and I am a student at McGill University, Majoring in Software Engineering and Minoring in Hispanic Studies. I am in my third year and I am a part of a variety of extracurriculars. I was the editor-in-chief of the <a target='_blank' href='http://www.plumbersfaucet.ca'>Plumber's Faucet</a>, and was and currently am the editor-in-chief of the Academic Journal <a target='_blank' href='http://issuu.com/clashsamcgill'>'Voces'</a> and was the Vice President of Information Technology of <a target='_blank' href='http://www.actuarialmcgill.ca'>McGill Students' Actuarial Association</a>.</br></br>I enjoy to spend my time listening to music, learning spanish and writing calligraphy.</br></br>I can be reached at <a target='_blank' href='mailto:faraz@farazoman.com'>faraz@farazoman.com</a>. Enjoy the rest of the site.</br>";
 //var welcomemsg = "Welcome to my website, to use the terminal type 'help' then press 'enter' for more commands."
-//var errmsg = "</br></br><div id='err'>Sorry</div> that is not valid command, refer to the valid commads below.";
+var errmsg = "</br></br><div id='err'>Sorry</div> that is not valid command, refer to the valid commads below.";
 //var proj_info = "</br></br>Hi there, thanks for the interest, my projects can be found at my <a target='_blank' href='http://www.github.com/farazoman'>github</a>. You will see the code for projects such as this terminal interface and others such as a video game in java and various hackathon mini projects.</br>"
 var version_info = "</br></br>" + version_ + "</br>This version is still in beta with many features still to be implemented. Source code can be viewed <a target='_blank' href='http://www.github.com/farazoman/terminal_webapp'>here</a></br>";
 var cmd = "";
@@ -20,6 +20,9 @@ var endline = "</br>";
 var blinkerOn = false;
 var inFocus = false;
 var remove_attr = "remove-cmd";
+var cmd_history = [];
+var hist_pos = 0;
+var cmd_text_tag = "cmd_text";
 
 function load(prompt_){
 
@@ -37,7 +40,6 @@ function load(prompt_){
             tmpName = i + "";
         }
         if(rm_attr){
-            //TODO create a NEW command that is used to remove a certain type of element.
             cmds[rm_attr] = new RemoveCmd(rm_attr);
             tmpCmd = new HTMLCmd("<cmd " + remove_attr + "=" + rm_attr + ">" + eles[i].innerHTML + "</cmd>");
         }
@@ -55,8 +57,8 @@ function load(prompt_){
 
     //add initial texts
     add_to_div(termdiv.getAttribute("welcome"));
-    prompt = endline + prompt_;
-    add_to_div(prompt);
+    prompt = endline + prompt_ ;
+    add_prompt();
     //turn on the blinker like in a normal terminal/text editor
     setInterval(function(){blinker()}, 500);
     //for setting the focus on the terminal
@@ -93,6 +95,17 @@ function add_to_div(text, isBlinker = false){
 
 }
 
+function update_cmd(text){
+    if(text != undefined){
+        cmd = text;
+        $("#term_area " + cmd_text_tag + ":last-child").html(cmd);
+    }
+}
+
+function add_prompt(){
+    add_to_div(prompt);
+    add_to_div("<"+cmd_text_tag+"></" + cmd_text_tag + ">", true);
+}
 
 var version = function version(){
     add_to_div(version_info);
@@ -147,6 +160,15 @@ function blinker(){
 
 }
 
+function add_to_hist(cmd){
+    if(cmd != ""){
+        cmd_history.unshift(cmd);
+    }
+    hist_pos = 0;
+    console.log(cmd_history );
+
+}
+
 //Event listener for keypressed
 document.addEventListener('keypress', function(event) {
     var termdiv = document.getElementById("term_area");
@@ -160,6 +182,7 @@ document.addEventListener('keypress', function(event) {
         	//process cmd here!!!
         	backspace(true);
             console.log(cmd);
+            add_to_hist(cmd);
         	try{
                 if(cmd){
         		  cmds[cmd].execute();
@@ -174,14 +197,12 @@ document.addEventListener('keypress', function(event) {
 
     		backspace(true);
         	cmd="";
-        	add_to_div(prompt); 
+        	add_prompt() 
         }
-        else{
+        else if (event.keyCode != 38 && event.keyCode != 40){
         	var ltr = String.fromCharCode(event.keyCode);
         	turnOffBlinker();
-        	add_to_div(ltr)
-
-        	cmd += ltr;
+        	update_cmd(cmd += ltr);
         	
         }
         termdiv.scrollTop = termdiv.scrollHeight;
@@ -189,31 +210,49 @@ document.addEventListener('keypress', function(event) {
     }
 });
 
+//
+var isUp = false;
 window.addEventListener('keydown', function(event) {
-  switch (event.keyCode) {
-    case 37: // Left
-            
-        break;
+    var temp_cmd;
+    switch (event.keyCode) {
+        case 37: // Left
+                
+            break;
 
-    case 38: // Up
-        break;
+        case 38: // Up
+            //TODO error checking for end of array
+            if(hist_pos < cmd_history.length && hist_pos >= 0){
+                turnOffBlinker();
+                update_cmd(cmd_history[hist_pos]);
+                console.log(cmd_history[hist_pos]);
+                hist_pos = hist_pos + 1;
+                isUp = true;
+            }
+            break;
 
-    case 39: // Right
-        break;
+        case 39: // Right
+            break;
 
-    case 40: // Down
-        break;
-  }
+        case 40: // Down
+            hist_pos = hist_pos - 1;
+            if(isUp){
+                isUp = false;
+                hist_pos = hist_pos - 1;
+            }
+            if(hist_pos < 0){
+                temp_cmd = "";
+                hist_pos = 0;
+            }else{
+                temp_cmd = cmd_history[hist_pos];
+            }
+            turnOffBlinker();
+            update_cmd(temp_cmd);
+            break;
+    }
 }, false);
 
-function left_cursor(){
-    var str = document.getElementById("term_area").innerHTML;
-
-}
-
 function backspace(blinkerOnly){
-    ////////////////TODO TODO TODO , it isn't working, needs to be working . wierd shit happens, check it out
-   	
+
    	if(blinkerOn && blinkerOnly){
         $("#term_area blink:last-child").remove();
 		blinkerOn = false;
@@ -223,8 +262,9 @@ function backspace(blinkerOnly){
         //$("#term_area span:last-child").remove();
         //  $("#term_area span:last-child").remove();
         $("#term_area blink:last-child").remove();
-        $("#term_area span:last-child").remove();
-        cmd = cmd.substring(0, cmd.length - 1);
+        
+        update_cmd(cmd.substring(0, cmd.length - 1));
+
     }
 
 }
@@ -233,7 +273,7 @@ function backspace(blinkerOnly){
 //Allow use of backspace to delete characters and not force it to go back a page
 $(document).unbind('keydown').bind('keydown', function (event) {
     var doPrevent = false;
-    if (event.keyCode === 8) {
+    if (event.keyCode === 8 || event.keyCode === 38 || event.keyCode === 40) {
         var d = event.srcElement || event.target;
         if ((d.tagName.toUpperCase() === 'INPUT' && 
              (
@@ -250,8 +290,10 @@ $(document).unbind('keydown').bind('keydown', function (event) {
         else {
             doPrevent = true;
         }
-        backspace(false);
-        turnOffBlinker();
+        if(event.keyCode === 8){
+            backspace(false);
+            turnOffBlinker();
+        }
     }
 
     if (doPrevent) {
